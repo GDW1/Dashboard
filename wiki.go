38b8cf0fb82e5666fdb32eb60bcdef0d12363377
GDW1 <Guest@Aporetos-MacBook-Pro.local>
2018-07-24 14:41:53 -0700

    Failing with the bootstrap dashboard
    Due to a problem with arrays the nice bootstrap interface was replaced
    with the most barebones possible layout

--- wiki.go
+++ wiki.go
@@ -15,11 +15,24 @@ type udata struct {
 }
 
 type data struct {
-	NumTest int
-	Test    string `json:"test"`
-	Status  bool   `json:"status"`
-	Last    bool   `json:"last"`
+	//	NumTest int
+	Test   string `json:"test"`
+	Status string `json:"status"`
+	Last   string `json:"last"`
 }
+
+type compiledData struct {
+	NumTest int
+	Test    []string
+	Status  []string
+	Last    []string
+}
+
+const tmpl = `
+	{{.Test}}
+	{{.Status}}
+	{{.Last}}
+`
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	jsonFile, err := os.Open("sample.json")
@@ -33,26 +46,23 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 
 	json.Unmarshal(byteValue, &udata)
 
-	//p := &data{NumTest: len(udata.UserData), Test: udata.UserData[0].Test, Status: udata.UserData[0].Status, Last: udata.UserData[0].Last}
-	f := make(map[int]interface{})
-	var t int = 0
+	var f compiledData
 	for i := 0; i < len(udata.UserData); i++ {
-		for e := 1; e < 3; e++ {
+		for e := 1; e <= 3; e++ {
 			if e == 1 {
-				f[t] = udata.UserData[i].Test
-				t++
+				f.Test = append(f.Test, udata.UserData[i].Test)
 			} else if e == 2 {
-				f[t] = udata.UserData[i].Status
-				t++
+				f.Status = append(f.Status, udata.UserData[i].Status)
 			} else if e == 3 {
-				f[t] = udata.UserData[i].Last
-				t++
+				f.Last = append(f.Status, udata.UserData[i].Last)
 			}
 		}
 	}
-	f[100] = len(udata.UserData)
-	b, _ := template.ParseFiles("dashboard.html")
-	b.Execute(w, udata)
+	f.NumTest = len(udata.UserData)
+	//b, _ := template.ParseFiles("Index.html")
+	t := template.Must(template.New("tmpl").Parse(tmpl))
+
+	t.Execute(w, f)
 
 }
 
